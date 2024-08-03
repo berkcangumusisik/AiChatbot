6698350312c43316d9e62aa54fd6f1cae17fffeb
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-08-03 20:36:12 +0300

    chore: Add ollama dependency to package.json and pnpm-lock.yaml

--- hono-ollama-backend/src/index.ts
+++ hono-ollama-backend/src/index.ts
@@ -1,24 +1,39 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import {serve} from '@hono/node-server'
+import {Hono} from 'hono'
+import ollama from 'ollama';
 
 const app = new Hono()
 
-app.get('/generate', (c) => {
-  return c.json({message:"Generate"})
+app.get('/', (c) => {
+    return c.text('Hello Hono!')
+})
+
+const messageHistory = [
+    {role: 'system', content: `You're a Turkish speaking assistant! Please write your responses in Turkish.`},
+]
+
+app.post('/generate', async (c) => {
+
+    const body = await c.req.json();
+
+    const prompt = body.prompt;
+
+    messageHistory.push( {role: 'user', content: prompt});
+
+    const response = await ollama.chat({
+        model: 'gemma2:2b',
+        messages:messageHistory,
+    })
+
+    console.log(response.message.content);
+    messageHistory.push( {role: 'assistant', content: response.message.content});
+    return c.json({message: response.message.content});
 })
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
 serve({
-  fetch: app.fetch,
-  port
+    fetch: app.fetch,
+    port
 })
-
-
-/**
- * olllama list : modelleri listeler.
-ollama run <model-adı> : modellei çalıştırır.
-/bye: ollamadan çıkar.
-ollama help: komutları gösterir.
- */
